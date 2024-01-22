8ccd7c0335bf1c9a14f31aadd75e8d034cf0ab18
jolivald <jolivald.jonathan@gmail.com>
2024-01-22 16:52:07 +0100

    a

---
+++ server/autoload.php
@@ -0,0 +1,11 @@
+<?php
+
+spl_autoload_register(function ($className){
+  $classPath = __DIR__ . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
+  if (file_exists($classPath)){
+    require_once($classPath);
+    return true;
+  } else {
+    return false;
+  }
+});
