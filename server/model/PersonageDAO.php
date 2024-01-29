ca59127db8367759e9e0b54f44c02df1811257f9
jolivald <jolivald.jonathan@gmail.com>
2024-01-29 16:52:17 +0100

    working entity & personage CRUD

---
+++ server/model/PersonageDAO.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace model;
+
+class PersonageDAO extends EntityDAO {
+  
+  private int $player;
+  private int $special;
+  private int $level;
+
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+  /**
+   * Get the value of special
+   */ 
+  public function getSpecial()
+  {
+    return $this->special;
+  }
+
+  /**
+   * Set the value of special
+   *
+   * @return  self
+   */ 
+  public function setSpecial($special)
+  {
+    $this->special = $special;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of level
+   */ 
+  public function getLevel()
+  {
+    return $this->level;
+  }
+
+  /**
+   * Set the value of level
+   *
+   * @return  self
+   */ 
+  public function setLevel($level)
+  {
+    $this->level = $level;
+
+    return $this;
+  }
+
+
+  /**
+   * Get the value of player
+   */ 
+  public function getPlayer()
+  {
+    return $this->player;
+  }
+
+  /**
+   * Set the value of player
+   *
+   * @return  self
+   */ 
+  public function setPlayer($player)
+  {
+    $this->player = $player;
+
+    return $this;
+  }
+}
