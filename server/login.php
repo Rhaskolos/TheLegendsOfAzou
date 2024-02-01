2098e45dec1276720b9c42f3ff952a346ee55917
Zack <zack@gmx.fr>
2024-02-01 13:57:55 +0100

    login fixed redirection menu

--- server/login.php
+++ server/login.php
@@ -1,5 +1,31 @@
 <?php
 
-// GET => formulaire HTML
+session_start();
 
-// POST => essaye de se connecter
+require_once 'autoload.php';
+
+use model\PlayerCRUD;
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $login = trim($_POST['login']);
+    $password = $_POST['password'];
+
+    try {
+        if (PlayerCRUD::login($login, $password)) {
+            $_SESSION['player_login'] = $login;
+            $_SESSION['is_logged_in'] = true;
+            header('Location: ../client/menu.html');
+            exit();
+        } else {
+            // Identifiants incorrects
+            echo "Identifiant ou mot de passe incorrects! . Veuillez réessayer.";
+        }
+    } catch (Exception $e) {
+        error_log($e->getMessage());
+        echo "Erreur lors de la connexion. Veuillez réessayer plus tard.";
+    }
+} else {
+    header('Location: login.html');
+    exit();
+}
